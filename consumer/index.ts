c2cf28395f954f234c28b8c3913be3bb3fe0b384
Lucas <lucasvalenteds@gmail.com>
2020-06-13 19:25:58 -0300

    Add draft of consumer using SSE

--- consumer/index.ts
+++ consumer/index.ts
@@ -1,4 +1,27 @@
+import * as HTTP from "http";
+import { v4 as UUID } from "uuid";
 import { Kafka, EachMessagePayload } from "kafkajs";
+
+type Message = {
+  id: string;
+  title: string;
+  content: string;
+  timestamp: string;
+};
+
+type SseEvent<T> = {
+  id: string;
+  event: string;
+  data: T;
+};
+
+function createSseEvent(message: Message): SseEvent<string> {
+  return {
+    id: UUID(),
+    event: "new-message",
+    data: JSON.stringify(message),
+  };
+}
 
 async function main() {
   try {
@@ -13,15 +36,41 @@ async function main() {
 
     const consumer = kafka.consumer({ groupId: topic });
 
-    await consumer.subscribe({ topic, fromBeginning: false });
+    const port = process.env.PORT;
+    const server = HTTP.createServer(async (request, response) => {
+      response.writeHead(200, {
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive",
+      });
 
-    await consumer.run({
-      eachMessage: async (payload: EachMessagePayload) => {
-        const message = payload.message.value.toString("utf-8");
+      await consumer.subscribe({ topic: topic });
 
-        console.debug("%s: Message received", new Date(), JSON.parse(message));
-      },
+      await consumer.run({
+        eachMessage: async (payload: EachMessagePayload) => {
+          const message: Message = JSON.parse(
+            payload.message.value.toString("utf-8")
+          );
+
+          const event = createSseEvent(message);
+
+          response.write(`id: ${event.id}\n`);
+          response.write(`event: ${event.event}\n`);
+          response.write(`data: ${event.data}\n\n`);
+        },
+      });
+
+      request.on("close", async () => {
+        await consumer.disconnect();
+        response.end();
+      });
     });
+
+    server
+      .listen(port)
+      .once("listening", () =>
+        console.debug("Server running on port %d", port)
+      );
   } catch (error) {
     console.error(error);
   }
