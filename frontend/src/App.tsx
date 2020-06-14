e834b79387b665dc25cafbd0319de6a93e932ebc
Lucas <lucasvalenteds@gmail.com>
2020-06-13 22:56:19 -0300

    Creating MessageService abstraction

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+export type Message = {
+  id: string;
+  title: string;
+  content: string;
+  timestamp: string;
+};
+
+export class MessageService {
+  private sse: EventSource;
+  private listener?: any;
+
+  public constructor(url: string) {
+    this.sse = new EventSource(url);
+  }
+
+  public listen<T>(event: string, fn: (data: T) => void): void {
+    this.listener = this.sse.addEventListener(event, (payload: any) => {
+      if (payload?.data !== undefined) {
+        fn(JSON.parse(payload.data));
+      }
+    });
+  }
+
+  public disconnect(event: string): void {
+    this.sse.removeEventListener(event, this.listener);
+  }
+}
+
+export type MessageListItemProps = {
+  message: Message;
+};
+
+export const MessageListItem: React.FC<MessageListItemProps> = (props) => {
+  return (
+    <>
+      <li key={props.message.id}>
+        <div>
+          <h2>{props.message.title}</h2>
+          <p>{props.message.content}</p>
+          <p>{props.message.timestamp}</p>
+        </div>
+      </li>
+    </>
+  );
+};
 
 export const App: React.FC = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+
   React.useEffect(() => {
-    const sse = new EventSource('http://localhost:3000/');
-    sse.onopen = (e) => {
-      console.debug('opened', e);
-    };
-    sse.onerror = (e) => {
-      console.error(e);
-    };
-    sse.addEventListener('new-message', (ev) => {
-      console.debug('ev', ev);
+    const service = new MessageService('http://localhost:8082');
+
+    service.listen<Message>('new-message', (message) => {
+      setMessages((previous: Message[]) => [...previous, message]);
     });
+
+    return () => service.disconnect('new-message');
   }, []);
 
-  return <div>React</div>;
+  return (
+    <>
+      <div>
+        <h1>Messages</h1>
+        <ul>
+          {messages.map((message) => (
+            <MessageListItem message={message} />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 };
